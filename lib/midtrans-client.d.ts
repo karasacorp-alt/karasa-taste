747bd802c2e2c1679bb744df3bf6c9a44365281e
karasacorp-alt <karasacorp@gmail.com>
2026-07-06 12:33:38 +0700

    Create midtrans-client.d.ts

---
+++ lib/midtrans-client.d.ts
@@ -0,0 +1,21 @@
+declare module "midtrans-client" {
+  export class Snap {
+    constructor(options: {
+      isProduction: boolean;
+      serverKey: string;
+      clientKey: string;
+    });
+    createTransaction(parameter: object): Promise<{
+      token: string;
+      redirect_url: string;
+    }>;
+  }
+
+  export class CoreApi {
+    constructor(options: {
+      isProduction: boolean;
+      serverKey: string;
+      clientKey: string;
+    });
+  }
+}
